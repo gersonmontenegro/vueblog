ba21d7727ae17ca0ebda63f3bfc6b9836c58fefa
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-30 05:39:57 +1300

    Implementing wygiwys plugin, and attach it the model in order to control the content

--- src/components/commons/Post.vue
+++ src/components/commons/Post.vue
@@ -1,23 +1,54 @@
 <template>
     <b-col md="6" style="background-color: white">
-        <ButtonBar enableButtonBar />
-        <div>
+        <ButtonBar @onChangeViewW="onChangeViewW" enableButtonBar />
+        <div >
             <h3>Post title</h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ....
-        </div>
+            <span v-if="show == 1" v-html="textFromProp"></span>
+        </div >
+        <wysiwyg class="wygiwys-style" v-if="show == 2" v-model="textFromProp" />
         <ViewMoreButton />
     </b-col>
 </template>
 
 <script>
+import Vue from "vue";
 import ButtonBar from "./ButtonBar";
 import ViewMoreButton from "./ViewMoreButton";
+import wysiwyg from "vue-wysiwyg";
+
+import "vue-wysiwyg/dist/vueWysiwyg.css";
+import "./css/Post.css";
+
+Vue.use(wysiwyg, {
+  hideModules: {
+    image: true,
+    table: true,
+    separator: true
+  }
+});
 
 export default {
   name: "Post",
+  data() {
+    return {
+      show: 1,
+      textFromProp: this.mainText
+    };
+  },
+  props: {
+    mainText: {
+      type: String,
+      required: true
+    }
+  },
   components: {
     ButtonBar,
     ViewMoreButton
+  },
+  methods: {
+    onChangeViewW(value) {
+      this.show = value;
+    }
   }
 };
 </script>
