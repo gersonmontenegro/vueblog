8bf17ad7a8e99ef3b4fbd3ffa1cf65bcdb287b31
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-30 05:40:30 +1300

    Implementing emitter from ButtonBar in order to show/hide the text editor

--- src/components/commons/ButtonBar.vue
+++ src/components/commons/ButtonBar.vue
@@ -1,7 +1,7 @@
 <template>
     <b-button-toolbar v-if="enableButtonBar" class="button-bar">
         <b-button-group >
-            <b-button class="defaut-button" variant="success">Edit</b-button>
+            <b-button class="defaut-button" variant="success" v-on:click="onChangeViewW">Edit</b-button>
             <b-button class="defaut-button" variant="danger">Remove</b-button>
         </b-button-group>
     </b-button-toolbar>
@@ -10,11 +10,25 @@
 <script>
 export default {
   name: "ButtonBar",
+  data() {
+    return {
+      show: 1
+    };
+  },
   props: {
     enableButtonBar: {
       type: Boolean,
       required: false,
       default: false
+    }
+  },
+  methods: {
+    showHide() {
+      this.show = this.show == 1 ? 2 : 1;
+      return this.show;
+    },
+    onChangeViewW() {
+      this.$emit("onChangeViewW", this.showHide());
     }
   }
 };
