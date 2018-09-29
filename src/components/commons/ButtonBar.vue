06fd1c1e309873a96fcba9172e02d11c34526292
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-29 19:11:22 +1200

    Creating ButtonBar component to keep edit and remove post buttons

---
+++ src/components/commons/ButtonBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <b-button-toolbar align-self="center">
+        <b-button-group >
+            <b-button class="defaut-button" variant="success">Edit</b-button>
+            <b-button class="defaut-button" variant="danger">Remove</b-button>
+        </b-button-group>
+    </b-button-toolbar>
+</template>
+
+<script>
+export default {
+  name: "ButtonBar"
+};
+</script>
