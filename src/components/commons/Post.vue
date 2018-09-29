a8e43ee5570362ffc0367f3c46d03190cfc88295
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-29 19:11:42 +1200

    Adding Post component to keep all the post data/elements

---
+++ src/components/commons/Post.vue
@@ -0,0 +1,23 @@
+<template>
+    <b-col md="6" style="background-color: gray">
+        <ButtonBar />
+        <div>
+            <h3>Post title</h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ....
+            <div>
+                <b-button class="defaut-button" variant="link">View more >></b-button>
+            </div>
+        </div>
+    </b-col>
+</template>
+
+<script>
+import ButtonBar from "./ButtonBar";
+
+export default {
+  name: "Post",
+  components: {
+    ButtonBar
+  }
+};
+</script>
