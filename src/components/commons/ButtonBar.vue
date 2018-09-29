9fd8c829d00c816a39b2d2879ae6b23b64024ccb
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-29 19:38:12 +1200

    Adding prop enableButtonBar to show or not the edit buttons bar

--- src/components/commons/ButtonBar.vue
+++ src/components/commons/ButtonBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <b-button-toolbar align-self="center">
+    <b-button-toolbar v-if="enableButtonBar" class="button-bar">
         <b-button-group >
             <b-button class="defaut-button" variant="success">Edit</b-button>
             <b-button class="defaut-button" variant="danger">Remove</b-button>
@@ -9,6 +9,19 @@
 
 <script>
 export default {
-  name: "ButtonBar"
+  name: "ButtonBar",
+  props: {
+    enableButtonBar: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
 };
 </script>
+
+<style scoped>
+.button-bar {
+  margin-top: 5px;
+}
+</style>
