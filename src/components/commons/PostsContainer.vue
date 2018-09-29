a30e7a3528bd493ad03850fa2414a9e824c3e7ba
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-29 19:12:35 +1200

    Create PostsContainer component to load all the posts into it

---
+++ src/components/commons/PostsContainer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <b-container>
+            <b-row>
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+import Post from "./Post";
+
+export default {
+  name: "PostsContainer",
+  components: {
+    Post
+  }
+};
+</script>
