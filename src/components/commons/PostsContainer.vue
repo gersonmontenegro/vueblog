aa85d87d0db3ec75e4dd4c02612f34f2e4a53920
Gerson Montenegro <montenegrogerson@gmail.com>
2018-09-30 05:42:45 +1300

    Implementing props mainText for the first time. In a while it will be a dynamic data

--- src/components/commons/PostsContainer.vue
+++ src/components/commons/PostsContainer.vue
@@ -2,10 +2,10 @@
     <div v-if="page == 1">
         <b-container>
             <b-row>
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                <Post mainText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo" />
+                <Post mainText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo" />
+                <Post mainText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo" />
+                <Post mainText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo" />
             </b-row>
         </b-container>
     </div>
