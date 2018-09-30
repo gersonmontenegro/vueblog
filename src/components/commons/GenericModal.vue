e6028f3518a4b1671584543dcc5669a5107a2134
Gerson Montenegro <montenegrogerson@gmail.com>
2018-10-01 10:23:49 +1300

    Adding general Modal in order to use in any case. But it will be replaced by notifications

---
+++ src/components/commons/GenericModal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-modal ref="theModalR" hide-footer title="Remove post" size="sm">
+      {{message}}
+      <p class="font-weight-bold">{{title}}</p>
+      <div v-if="!hideFoot">
+        <b-button variant="primary" @click="onCloseModal">Ok</b-button>
+      </div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GenericModal",
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: ""
+    },
+    message: {
+      type: String,
+      required: true,
+      default: ""
+    },
+    hideFoot: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  methods: {
+    openModal() {
+      this.$refs.theModalR.show();
+    },
+    onCloseModal() {
+      this.$refs.theModalR.hide();
+    }
+  }
+};
+</script>
